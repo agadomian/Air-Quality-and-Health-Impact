07dd55365cc480e55992b6ed00795129bf576cdc
adomian <agata.domian@thincats.com>
2024-06-24 22:35:12 +0100

    Code Update

--- air_quality.sql
+++ air_quality.sql
@@ -6,7 +6,7 @@ min(aqi)
 , avg(aqi)
 , max(aqi)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 500
 
 -- PM10 
 select 
@@ -14,7 +14,7 @@ min(PM10)
 , avg(PM10)
 , max(PM10)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 300
 
 -- PM2_5 
 select 
@@ -22,7 +22,7 @@ min(PM2_5)
 , avg(PM2_5)
 , max(PM2_5)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 200
 
 -- NO2 
 select 
@@ -30,7 +30,7 @@ min(NO2)
 , avg(NO2)
 , max(NO2)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 200
 
 -- SO2 
 select 
@@ -38,7 +38,7 @@ min(SO2)
 , avg(SO2)
 , max(SO2)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 100
 
 -- O3 
 select 
@@ -46,7 +46,7 @@ min(O3)
 , avg(O3)
 , max(O3)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 300
 
 -- Temperature
 select 
@@ -54,7 +54,7 @@ min(Temperature)
 , avg(Temperature)
 , max(Temperature)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between -10 and 40
 
 -- Humidity
 select 
@@ -62,7 +62,7 @@ min(Humidity)
 , avg(Humidity)
 , max(Humidity)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 10 and 100
 
 -- WindSpeed
 select 
@@ -70,7 +70,7 @@ min(WindSpeed)
 , avg(WindSpeed)
 , max(WindSpeed)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 20
 
 -- RespiratoryCases
 select 
@@ -78,7 +78,7 @@ min(RespiratoryCases)
 , avg(RespiratoryCases)
 , max(RespiratoryCases)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 1 and 23 
 
 -- CardiovascularCases
 select 
@@ -86,7 +86,7 @@ min(CardiovascularCases)
 , avg(CardiovascularCases)
 , max(CardiovascularCases)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 14
 
 -- HospitalAdmissions
 select 
@@ -94,7 +94,7 @@ min(HospitalAdmissions)
 , avg(HospitalAdmissions)
 , max(HospitalAdmissions)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 12
 
 -- HealthImpactScore
 select 
@@ -102,7 +102,7 @@ min(HealthImpactScore)
 , avg(HealthImpactScore)
 , max(HealthImpactScore)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 22 and 100
 
 -- HealthImpactClass
 select 
@@ -110,16 +110,16 @@ min(HealthImpactClass)
 , avg(HealthImpactClass)
 , max(HealthImpactClass)
 from 
-air_quality.air_quality_health_impact_data; -- seems ok 
+air_quality.air_quality_health_impact_data; -- values between 0 and 4
 
--- since all the variables seem logical and there's no unusual values (e.g. temeperature of 200 degrees celcius), let's proceed to a deeper analysis
+/* since all the variables seem logical and there's no unusual values (e.g. temeperature of 200 degrees celcius), let's proceed to dive into a deeper analysis
 
--- let's first see for each of the health class:
--- number of hispital addmisions 
--- number of respirtory/cardiovascular cases admitted 
--- average health score
+ let's first see for each of the health class:
+ -  number of hispital addmisions 
+ - number of respirtory/cardiovascular cases admitted 
+ - average health score */
 
-select
+select 
 HealthImpactClass as health_class
 , avg(HealthImpactScore) as average_health_score
 , sum(RespiratoryCases) as respiratory_cases 
@@ -129,37 +129,43 @@ from
 air_quality.air_quality_health_impact_data aqhid
 group by 1;
 
-select * from air_quality.summary;
-/* interestingly, the average health score is very similar for 'very low' class and 'very high' class
-   that might indicate how important and influencial air quality & other factors are on our health */
+/* the averages for class 2 and class 3 look odd. class 2 should be withing range 40-60, and class 3 should be within range 20-40, howeber, both of them have an averag>60.
+   let's see what could have gone wrong */
 
--- now let's look at the geogical split
-
-select 
-HealthImpactClass as health_class
-, avg(Temperature) as temperature
-, avg(Humidity) as humidity 
-, avg(WindSpeed) as wind_speed
+select
+HealthImpactClass as health_class 
+, min(HealthImpactScore) as min_score
+, max(HealthImpactScore) as max_score
+, sum(case 
+	when HealthImpactClass=0 and (HealthImpactScore<80) then 1
+	when HealthImpactClass=1 and (HealthImpactScore>=80 or HealthImpactScore<60) then 1
+	when HealthImpactClass=2 and (HealthImpactScore>=60 or HealthImpactScore<40) then 1 
+	when HealthImpactClass=3 and (HealthImpactScore>=40 or HealthImpactScore<20) then 1 
+	when HealthImpactClass=4 and (HealthImpactScore>=20) then 1
+	else 0 end) as outside_of_range
+, (sum(case 
+	when HealthImpactClass=0 and (HealthImpactScore<80) then 1
+	when HealthImpactClass=1 and (HealthImpactScore>=80 or HealthImpactScore<60) then 1
+	when HealthImpactClass=2 and (HealthImpactScore>=60 or HealthImpactScore<40) then 1 
+	when HealthImpactClass=3 and (HealthImpactScore>=40 or HealthImpactScore<20) then 1 
+	when HealthImpactClass=4 and (HealthImpactScore>=20) then 1
+	else 0 end)/count(*))*100 as outside_of_range_pct
+, count(*)
 from 
-air_quality.air_quality_health_impact_data aqhid
+air_quality.air_quality_health_impact_data aqhid 
 group by 1;
 
-/* there doesn't seem to be a big difference between all health classes.
-   maybe later on, when looking at the air quality metrics, something new will come up */
+/* the HealthImpactClass seems to have some incorrect records, hence it will be ignored for now and substituted with a corrected version */
 
 select 
-HealthImpactClass 
-, min(AQI) as min_aqi
-, max(AQI) as max_aqi
-, avg(AQI) as avg_aqi
-from 
-air_quality.air_quality_health_impact_data aqhid;
-
-/* the range of AQI is between 0 and 500 */
-
-select 
-HealthImpactClass 
-, avg(AQI) as avg_aqi
+case 
+	when (HealthImpactScore>=80) then 0
+	when (HealthImpactScore<80 and HealthImpactScore>=60) then 1
+	when (HealthImpactScore<60 and HealthImpactScore>=40) then 2
+	when (HealthImpactScore<40 and HealthImpactScore>=20) then 3 
+	when (HealthImpactScore<20) then 4
+	end as health_class
+, avg(HealthImpactScore) as average_health_score
 , sum(RespiratoryCases) as respiratory_cases 
 , sum(CardiovascularCases) as cardiovascular_cases 
 , sum(HospitalAdmissions) as hospital_admissions 
@@ -167,16 +173,104 @@ from
 air_quality.air_quality_health_impact_data aqhid
 group by 1;
 
+/* now let's look at the geogical split.
+   some of the questions that come to mind are:
+   - does are quality index have some strong indicators among geological metrics? 
+   - is there a connection between air quality and number of hospital admissions?
+ */
+
 select 
-HealthImpactClass 
-, min(AQI) as min_aqi
-, max(AQI) as max_aqi
-, avg(HealthImpactScore)
-, min(HealthImpactScore)
-, max(HealthImpactScore)
-from air_quality.air_quality_health_impact_data aqhid 
+case 
+	when (HealthImpactScore>=80) then 0
+	when (HealthImpactScore<80 and HealthImpactScore>=60) then 1
+	when (HealthImpactScore<60 and HealthImpactScore>=40) then 2
+	when (HealthImpactScore<40 and HealthImpactScore>=20) then 3 
+	when (HealthImpactScore<20) then 4
+	end as health_class
+, avg(aqi) as avg_aqi
+, min(aqi) as min_aqi
+, max(aqi) as max_aqi
+, avg(Temperature) as temperature
+, avg(Humidity) as humidity 
+, avg(WindSpeed) as wind_speed
+, sum(RespiratoryCases) as respiratory_cases 
+, sum(CardiovascularCases) as cardiovascular_cases 
+, sum(HospitalAdmissions) as hospital_admissions 
+from 
+air_quality.air_quality_health_impact_data aqhid
+group by 1; 
+
+/*there doesn't seem to be much difference between temperatures, humidity abd wind speed and the health clas.
+  there is, however, a correlation between health class and the average air quality index. 
+  that makes sense - the better the air quality, the highest the health class, the lower the hispital admission numbers.
+ */
+ 
+-- let's look at the concetrations of different particules and their impact on air quality, hence air 
+
+select 
+case 
+	when (HealthImpactScore>=80) then 0
+	when (HealthImpactScore<80 and HealthImpactScore>=60) then 1
+	when (HealthImpactScore<60 and HealthImpactScore>=40) then 2
+	when (HealthImpactScore<40 and HealthImpactScore>=20) then 3 
+	when (HealthImpactScore<20) then 4
+	end as health_class
+, avg(aqi) as AQI 
+, avg(pm10) as PM10
+, avg(PM2_5) as PM2_5 
+, avg(no2) as NO2 
+, avg(so2) as SO2
+, avg(o3) as O3
+from 
+air_quality.air_quality_health_impact_data aqhid 
 group by 1;
 
 select 
-*
-from air_quality.air_quality_health_impact_data aqhid ;
+case
+	when Temperature<0 then 1
+	when Temperature>=0 and Temperature<15 then 2
+	when Temperature>=15 and Temperature<25 then 3
+	when Temperature>=25 then 4
+	else '?'
+	end as temperature_rank
+, case 
+	when Temperature<0 then '<0'
+	when Temperature>=0 and Temperature<15 then '0-15'
+	when Temperature>=15 and Temperature<25 then '15-25'
+	when Temperature>=25 then '>25'
+	else '?'
+	end as temperature
+, avg(pm10) as PM10
+, avg(PM2_5) as PM2_5 
+, avg(no2) as NO2 
+, avg(so2) as SO2
+, avg(o3) as O3
+from 
+air_quality.air_quality_health_impact_data aqhid
+group by 1,2
+order by 1 asc;
+
+select 
+case
+	when Humidity<20 then 1
+	when Humidity>=20 and Humidity<50 then 2
+	when Humidity>=50 and Humidity<80 then 3
+	when Humidity>=80 then 4
+	else '?'
+	end as humidity_rank
+, case
+	when Humidity<20 then '10-20%'
+	when Humidity>=20 and Humidity<50 then '20-50%'
+	when Humidity>=50 and Humidity<80 then '50-80%'
+	when Humidity>=80 then '80-100%'
+	else '?'
+	end as humidity
+, avg(pm10) as PM10
+, avg(PM2_5) as PM2_5 
+, avg(no2) as NO2 
+, avg(so2) as SO2
+, avg(o3) as O3
+from 
+air_quality.air_quality_health_impact_data aqhid
+group by 1,2
+order by 1 asc;
